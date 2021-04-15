1286a9936aaf6c36287a6d0e6e4f7f4061979041
Zach Andrews <ztandrews18@sbcglobal.net>
2021-04-14 22:40:14 -0400

    V 1.0.3

--- HitterZScoreRadar.py
+++ HitterZScoreRadar.py
@@ -6,8 +6,8 @@ Create a Radar chart that plots an MLLB hitters Z-Scores for certain statistics.
 The visualization will give us a good idea of how the hitter comaprees to the average MLB hitter.
 
 The stats we will be using are:
-    - wobacon
-    - xwobacon
+    - wOBA
+    - xwOBA
     - OPS
     - Hard Hit %
     - Barrel %
@@ -17,9 +17,9 @@ The stats we will be using are:
 
 STEPS TO RUN
 1. Get the stats from a csv from this link:
-    https://baseballsavant.mlb.com/leaderboard/custom?year=2021&type=batter&filter=&sort=3&sortDir=desc&min=q&selections=b
-    _total_pa,on_base_plus_slg,wobacon,xwobacon,exit_velocity_avg,barrel_batted_rate,hard_hit_percent,
-    &chart=false&x=b_total_pa&y=b_total_pa&r=no&chartType=beeswarm
+    https://baseballsavant.mlb.com/leaderboard/custom?year=2021&type=batter&filter=
+    &sort=5&sortDir=asc&min=q&selections=b_total_pa,on_base_plus_slg,woba,xwoba,
+    exit_velocity_avg,barrel_batted_rate,hard_hit_percent,&chart=false&x=b_total_pa&y=b_total_pa&r=no&chartType=beeswarm
     
     Make sure Qualified hitters is selected and press 'Download CSV'
 
@@ -37,64 +37,73 @@ from math import pi
 
 
 #Enter the name of the file you downloaded from Baseball Savant
-fileName = 'radarstats2.csv'
+fileName = 'radarstats4.csv'
 #Change this to get the player's data that we want to chart
-playerToChart = 'Aaron Judge'
+playerToChart = 'Mike Trout'
 
 #Read CSV file
 data = pd.read_csv (r'C:/Users/ztand/Desktop/Python/'+fileName)
 data['name'] = data[' first_name'] + ' ' + data['last_name']
 
 #Get the means of each column
-wobaconmean = data['wobacon'].mean()
+wobamean = data['woba'].mean()
 evamean = data['exit_velocity_avg'].mean()
 bbrmean = data['barrel_batted_rate'].mean()
-xwobaconmean = data['xwobacon'].mean()
+xwobamean = data['xwoba'].mean()
 opsmean = data['on_base_plus_slg'].mean()
 hhpmean = data['hard_hit_percent'].mean()
 
 #Get the standard deviation of each column
-wobaconsd = data['wobacon'].std()
+wobasd = data['woba'].std()
 evasd = data['exit_velocity_avg'].std()
 bbrsd = data['barrel_batted_rate'].std()
-xwobaconsd = data['xwobacon'].std()
+xwobasd = data['xwoba'].std()
 opssd = data['on_base_plus_slg'].std()
 hhpsd = data['hard_hit_percent'].std()
 
 #Create z-score columns for each player and each stat
 #Z-Score = (observed value - mean of the sample) / standard deviation of the sample
-data['wobacon_zscore'] = (data['wobacon'] - wobaconmean) /wobaconsd
+data['woba_zscore'] = (data['woba'] - wobamean) /wobasd
 data['eva_zscore'] = (data['exit_velocity_avg'] - evamean)/evasd
 data['bbr_zscore'] = (data['barrel_batted_rate']- bbrmean)/bbrsd
-data['xwobacon_zscore'] = (data['xwobacon'] - xwobaconmean) /xwobaconsd
+data['xwoba_zscore'] = (data['xwoba'] - xwobamean) /xwobasd
 data['ops_zscore'] = (data['on_base_plus_slg'] - opsmean) /opssd
 data['hhp_zscore'] = (data['hard_hit_percent'] - hhpmean) /hhpsd
 
-
+#Create a column for each player that is a sum of their Z-Scores. This will determine the color of their chart
+data['zsc_sum'] = (data['hhp_zscore']+data['ops_zscore']+data['xwoba_zscore']+data['bbr_zscore']+data['eva_zscore']+data['woba_zscore'] )
+zscavg = data['zsc_sum'].mean()
 
 #Get the Z-Scores and other stats from the DataFrame
 player = data[data['name'].str.contains(playerToChart)]
 ops = float(player['ops_zscore'])
-wobacon = float(player['wobacon_zscore'])
-xwobacon = float(player['xwobacon_zscore'])
+woba = float(player['woba_zscore'])
+xwoba = float(player['xwoba_zscore'])
 eva = float(player['eva_zscore'])
 bbr = float(player['bbr_zscore'])
 hhp = float(player['hhp_zscore'])
 year = str(player['year'])
 pa = str(player['b_total_pa'])
+zum = str(player['zsc_sum'])
 
-stats = ['OPS', 'wOBACON', 'xwOBACON', 'AVG. EV', 'BARREL %','HARD HIT %']
-values = [ops,wobacon,xwobacon,eva,bbr,hhp]
+stats = ['OPS', 'wOBA', 'xwOBA', 'AVG. EV', 'BARREL %','HARD HIT %']
+values = [ops,woba,xwoba,eva,bbr,hhp]
 
 #Style and plot the Polar Chart
+zscoresum = (ops+woba+xwoba+eva+bbr+hhp)
+#The color of the chart logic. If the players sum of Z-Scores is more than the mean of the sum of every players Z-Scores, the chart will be blue. If it is less, the chart will be red
+if zscoresum < zscavg:
+    col='#ff4c4c'
+elif zscoresum > zscavg:
+    col='#123ed6'
 fig = plt.figure(figsize=(25,25))
 ax = plt.subplot(polar="True")
 N= len(stats)
 values+= values[:1]
 angles = [n/float(N) * 2 * pi for n in range(N)]
 angles += angles[:1]
-plt.polar(angles, values, marker='.')
-plt.fill(angles,values,alpha=.3)
+plt.polar(angles, values,col,marker='.')
+plt.fill(angles,values,col,alpha=.3)
 plt.xticks(angles[:-1], stats)
 ax.set_rlabel_position(30)
 plt.yticks([-3,-2,-1,0,1,2,3], color='grey',size=20)
